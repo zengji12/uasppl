dbd1dc5793b55122ef6c273eff144efdda8b3f3a
zengji12 <rezaardiansyahyudhanegara@gmail.com>
2025-03-12 10:55:44 +0700

    new conntact

--- tests/03_test_new_contact.py
+++ tests/03_test_new_contact.py
@@ -6,6 +6,15 @@ from selenium.webdriver.support import expected_conditions as EC
 BASE_URL = "http://localhost:8000/"
 
 def test_new_contact(driver):
+    driver.get(BASE_URL + "login.php")
+    
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("admin")
+    driver.find_element(By.NAME, "password").send_keys("nimda666!")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), \"OK I'm sign in\")]"))
+    ).click()
+
     driver.get(BASE_URL + "create.php")
 
     driver.find_element(By.NAME, "name").send_keys("Reza")
