271211ac64afafaba652fcadb982f4675ad7c260
zengji12 <rezaardiansyahyudhanegara@gmail.com>
2025-03-12 10:24:48 +0700

    a

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function pdo_connect(){
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'badcrud';
+    $DATABASE_NAME = 'damncrud';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
